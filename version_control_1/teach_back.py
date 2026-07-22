da5471162b3b19ca7340b9ea35ebdb1071d67e05
Rocket-Sandrew <ad.buissness1224@gmail.com>
2026-07-22 11:22:17 -0400

    Add temprory file to simulate pushing changes to GitHub

---
+++ version_control_1/teach_back.py
@@ -0,0 +1,27 @@
+#print(x)
+
+#x = 7
+# try:
+#     print(x)
+# except:
+#     print("An error happened")
+# else:
+#     print("Nothing bad happened")
+# finally:
+#     print("The program is done")
+
+#number_to_use = int(input("Please enter a number you wish to divide by 2: "))
+
+# number_to_use = input("Please enter a number you wish to divide by 2: ")
+# try:
+#     number_to_use = int(number_to_use)
+#     if number_to_use < 0:
+#         raise TypeError()
+# except ValueError:
+#     print("Sorry! that doesn't work, please only enter a number")
+# except TypeError:
+#     print("Only positive numbers!")
+# else:
+#     print(number_to_use/2)
+# finally:
+#     print("Program finished")
